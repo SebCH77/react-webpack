75b0340c4c2133e5cb038de9160b551603ec12d4
Sebastian Carrillo <scarrillo@lya-electronic.com>
2022-03-20 21:37:12 -0500

    [ADD] CSS Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js", //Punto de entrada principal
@@ -24,6 +25,10 @@ module.exports = {
         test: /\.html$/,
         use: [{ loader: "html-loader" }],
       },
+      {
+        test: /\.(css|scss)$/,
+        use: [/* MiniCssExtractPlugin.loader,  */ "style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
@@ -31,6 +36,9 @@ module.exports = {
       inject: true,
       template: "./public/index.html",
       filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
     }),
   ],
   devServer: {
